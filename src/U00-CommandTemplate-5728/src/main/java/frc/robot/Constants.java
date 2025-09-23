ff04df2b8c1af939bb0c9b60919c9ba2725db3d0
tdsuser <45298089+tdsuser@users.noreply.github.com>
2025-09-22 21:03:39 -0700

    Added a few prototypes to experiment with while exploring why swerve drive is not working as expected.

--- src/U00-CommandTemplate-5728/src/main/java/frc/robot/Constants.java
+++ src/U00-CommandTemplate-5728/src/main/java/frc/robot/Constants.java
@@ -15,7 +15,7 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
-    public static final double deadband = .1;
+    public static final double deadband = 0.25;
   }
 
   public static class SwerveConstants {
