aa1d75bec3e10a3eadb446b5e5c900dd5cde5ed4
Team 5728 <frc.robotics.5728@gmail.com>
2026-03-12 17:09:57 -0700

    Working intake, launcher, and drive mapping

--- src/kitbot-baseline/src/main/java/frc/robot/Constants.java
+++ src/kitbot-baseline/src/main/java/frc/robot/Constants.java
@@ -42,8 +42,8 @@ public final class Constants {
 
   public static final class FuelConstants {
     // Motor controller IDs for Fuel Mechanism motors
-    public static final int FEEDER_MOTOR_ID_SPARKMAX = 21;
-    public static final int INTAKE_LAUNCHER_MOTOR_ID_SPARKMAX = 22;
+    public static final int FEEDER_MOTOR_ID_SPARKMAX = 22;
+    public static final int INTAKE_LAUNCHER_MOTOR_ID_SPARKMAX = 21;
 
     public static final int FEEDER_MOTOR_ID_SPX = 6;
     public static final int INTAKE_LAUNCHER_MOTOR_ID_SPX = 5;
@@ -57,9 +57,9 @@ public final class Constants {
     // See the Software Guide for tuning information
     public static final double INTAKING_FEEDER_VOLTAGE = -12;
     public static final double INTAKING_INTAKE_VOLTAGE = 10;
-    public static final double LAUNCHING_FEEDER_VOLTAGE = 9;
+    public static final double LAUNCHING_FEEDER_VOLTAGE = -9;
     public static final double LAUNCHING_LAUNCHER_VOLTAGE = 10.6;
-    public static final double SPIN_UP_FEEDER_VOLTAGE = -6;
+    public static final double SPIN_UP_FEEDER_VOLTAGE = 6;
     public static final double SPIN_UP_SECONDS = 1;
   }
 
